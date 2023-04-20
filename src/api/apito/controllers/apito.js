d24e3f61e78fcb98319eab20f20a7107a24173f9
Alexei Garban <garban.valdeon@gmail.com>
2023-04-20 11:41:13 +0200

    first commit

---
+++ src/api/apito/controllers/apito.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * apito controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::apito.apito');
---
+++ src/api/apito/services/apito.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * apito service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::apito.apito');
