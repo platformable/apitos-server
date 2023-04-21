7b1cb4ba80894e2977bec3c88be306434f2b830c
Alexei Garban <garban.valdeon@gmail.com>
2023-04-21 12:53:36 +0200

    new option added and updated pdf text

--- src/api/pdf/controllers/pdf.js
+++ src/api/pdf/controllers/pdf.js
@@ -1,5 +1,10 @@
 'use strict';
 const PDFDocument = require('pdfkit');
+const pdfData = require('./pdfData')
+
+
+const pdfTitle = 'FACT Licence'
+const pdtSubtitle = 'Your API Terms are Fair, Transparent and Trustworthy. It’s a FACT.'
 /**
  * A set of functions called "actions" for `pdf`
  */
@@ -8,13 +13,29 @@ module.exports = {
   createPdf: async (ctx, next) => {
     try {
       // ctx.body = 'ok';
-      console.log("pdf")
+      
       const doc = new PDFDocument();
-      doc.fontSize(25).text('Hola, Platformable!');
+
+
+      doc.font('Helvetica-Bold').fontSize(26).text(pdfData.pdfTitle);
+      doc.fontSize(16).fillColor('gray').text(pdfData.pdtSubtitle);
+      doc.moveDown()
+      doc.fontSize(23).fillColor('black').text('0 Intro',{fontWeight: 'bold'})
+      doc.moveDown()
+      doc.fontSize(17).fillColor('black').text('0.1 Abstract',{fontWeight: 'bold'})
+      doc.font('Helvetica').fontSize(11).text(pdfData.abstractText,{align:'justify'})
+      doc.moveDown()
+      doc.font('Helvetica-Bold').fontSize(17).fillColor('black').text('0.2 Legal notice & Disclaimer',{fontWeight: 'bold'})
+      doc.font('Helvetica').fontSize(11).text(pdfData.legalAndDisclaimer,{align:'justify'})
+
+
+
+
       ctx.set('Content-Type', 'application/pdf');
       ctx.set('Content-Disposition', 'attachment; filename="archivo.pdf"');
       doc.pipe(ctx.res);
       doc.end();
+      console.log("end pdf")
     } catch (err) {
       ctx.body = err;
     }
---
+++ src/api/pdf/controllers/pdfData.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+    pdfTitle:'FACT Licence',
+    pdtSubtitle:'Your API Terms are Fair, Transparent and Trustworthy. It’s a FACT.',
+     abstractText:`The FACT (for FAIR API Commitment to Trust) licence is a customizable Contract that expands upon traditional API Terms of Service (ToS). Its purpose is to promote API usage  and  allow API Users to develop interoperable products, whether they be new APIs or other types of products.
+
+     Using a standardized set of contracts that are widely accepted as safe by the community, the Core API Provider and API Users can reduce transaction costs.  The licence also aims to raise the users’ awareness of key licensing issues that the parties need to address. Furthermore, it helps companies to define a clear, unambiguous and legally-binding framework that encourages the development of products interoperable with their technologies. This can lead tot the creation of high-value products and services in a healthy, trustworthy and safe digital infrastructure ecosystem.
+     
+     The FACT Contract is drafted neutrally to cover all rights related to API usage including Intellectual Property rights such as patents, copyrights and trade secrets, regardless of their validity. The standard terms of the FACT licence, along with all other specific binding documents, listed in the “API contractual commitments summary”, make up the entire contract.
+     `,
+    legalAndDisclaimer:`As a result of our research, the FACT licence is distributed under a Creative Commons Attribution 4.0 International Public Contract (CC BY 4.0). This original Contract is a pilot based on the INTILA (Interoperability Information Licence Agreement), and will evolve in response to feedback and usage.
+    
+    We are not a law firm and do not offer legal services or advice. The Distribution of this Contract does not create a lawyer-client or any other kind of relationship. We provide this Contract and related information on an “as-is” basis, without warranties regarding the Contract, any material licensed under its terms and conditions, or any related information. We disclaim all liability for damages resulting from their use to the fullest extent possible.
+    ` 
+};
